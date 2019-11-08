a62eb35c67ebd837e2a28142fb5c71694d00d72e
jordilg13 <jordillopis00@gmail.com>
2019-11-08 12:47:20 +0100

    thumbnail in all videos list with the redirection and all my code comented

--- js/video_controls.js
+++ js/video_controls.js
@@ -4,31 +4,38 @@ theVideo = document.querySelector("#play-video");
 // detecta cuando se termina el video y reproduce el siguiente
 theVideo.addEventListener("ended", changeVideo);
 
+// reproduccir un video
 function playVideo() {
     theVideo.play();
 }
 
+// pausar un video
 function pauseVideo() {
     theVideo.pause();
 }
 
+// detener un video y reiniciar sin autoreproducir
 function stopVideo() {
     theVideo.currentTime = 0;
     theVideo.pause();
 }
 
+// hacer retroceder el video hasta el principio
 function rewindVideo() {
     theVideo.currentTime = 0;
 }
 
+// avanza el video 10 segundos
 function avanceSeconds() {
     theVideo.currentTime = theVideo.currentTime + 10;
 }
 
+// hace retroceder el video 10 segundos
 function rewindSeconds() {
     theVideo.currentTime = theVideo.currentTime - 10;
 }
 
+// funcion que cambia de video al siguiente o al anterior dependiendo del parametro(refactorizacion de dos funciones en una)
 // p = previous
 // n = next (default)
 function changeVideo(param = "n") {
