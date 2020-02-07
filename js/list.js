16b35cdf23172a02386a1750b5ca59f19983d658
Zar21 <ivanperezfita@gmail.com>
2020-02-07 18:36:53 +0100

    JSON FINALE

--- js/list.js
+++ js/list.js
@@ -6,7 +6,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Tutorial basico para descargar chrome",
             "num_rep": 25,
-            "description": "La forma mas facil de descargar chrome en 2020 mediante mega"
+            "description": "La forma mas facil de descargar chrome en 2020 mediante mega",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "movie_clip2",
@@ -14,7 +15,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Yo y mi amigo",
             "num_rep": 75,
-            "description": "Hola amiguetes en el video de hoy les presentare a mi amigo espero que lo disfruten y suscribanse y denle a like ^^"
+            "description": "Hola amiguetes en el video de hoy les presentare a mi amigo espero que lo disfruten y suscribanse y denle a like ^^",
+            "media": "movie_clip2.mp4"
         },
         {
             "id": "audio_clip1",
@@ -22,7 +24,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "DJANGO DISK 2020",
             "num_rep": 15,
-            "description": "Los mejores temas del super dj DJANGO en 2020 ya que hemos pasado de decada(bueno no sera en 2021)"
+            "description": "Los mejores temas del super dj DJANGO en 2020 ya que hemos pasado de decada(bueno no sera en 2021)",
+            "media": "audio_clip1.mp3"
         },
         {
             "id": "movie_clip3",
@@ -30,7 +33,8 @@ const media = {
             "image": "poster.jpg",
             "text": "No muerdas los cables",
             "num_rep": 4354,
-            "description": "En este video se mostrara como el individuo Raül Ojeda Gandia se dedica a morder todos los cables existentes"
+            "description": "En este video se mostrara como el individuo Raül Ojeda Gandia se dedica a morder todos los cables existentes",
+            "media": "movie_clip3.mp4"
         },
         {
             "id": "audio_clip2",
@@ -38,7 +42,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Jordi cantando cumbia con su colega argentino",
             "num_rep": 234,
-            "description": "Jordi Llopis Garcia cantando cumbia con su colega usando pipas ya que el tio es Argetino y le gusta DEMASIADO la cumbia"
+            "description": "Jordi Llopis Garcia cantando cumbia con su colega usando pipas ya que el tio es Argetino y le gusta DEMASIADO la cumbia",
+            "media": "audio_clip2.mp3"
         },
         {
             "id": "movie_clip4",
@@ -46,7 +51,8 @@ const media = {
             "image": "poster.jpg",
             "text": "El brayan va a visitar a juanan (termina mal)",
             "num_rep": 64,
-            "description": "El combate definitivo gran final el imparable Brayan vs el gran Juanan (Habra muchas victimas)"
+            "description": "El combate definitivo gran final el imparable Brayan vs el gran Juanan (Habra muchas victimas)",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "movie_clip5",
@@ -54,7 +60,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Me voy por la noche y pasa esto",
             "num_rep": 2,
-            "description": "Jugamos por la noche y pasa una cosa que absolutamente nadie esperaria que pasara"
+            "description": "Jugamos por la noche y pasa una cosa que absolutamente nadie esperaria que pasara",
+            "media": "movie_clip2.mp4"
         },
         {
             "id": "audio_clip3",
@@ -62,7 +69,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Disco de Eminem",
             "num_rep": 444,
-            "description": "El ultimo disco del cantante Eminem creado en 2020 y con videoclip incluido de la cancion Darkness. Espero que lo disfruten"
+            "description": "El ultimo disco del cantante Eminem creado en 2020 y con videoclip incluido de la cancion Darkness. Espero que lo disfruten",
+            "media": "audio_clip3.mp3"
         },
         {
             "id": "movie_clip6",
@@ -70,7 +78,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Coche blanco",
             "num_rep": 432,
-            "description": "Unboxing de mi nuevo coche blanco"
+            "description": "Unboxing de mi nuevo coche blanco",
+            "media": "movie_clip3.mp4"
         },
         {
             "id": "audio_clip4",
@@ -78,7 +87,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Mejores canciones del 2002",
             "num_rep": 876,
-            "description": "Los mejores temazos de el 2002 con las canciones favoritas de tu padre"
+            "description": "Los mejores temazos de el 2002 con las canciones favoritas de tu padre",
+            "media": "audio_clip1.mp3"
         },
         {
             "id": "audio_clip5",
@@ -86,7 +96,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Monologo loulogio",
             "num_rep": 7685,
-            "description": "Gran monologo del catalan Loulogio que es youtuber y dibujante"
+            "description": "Gran monologo del catalan Loulogio que es youtuber y dibujante",
+            "media": "audio_clip2.mp3"
         },
         {
             "id": "movie_clip7",
@@ -94,7 +105,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Momos",
             "num_rep": 235,
-            "description": "Video donde se muestran una gran cantidad de Momos de toda nuestra historia"
+            "description": "Video donde se muestran una gran cantidad de Momos de toda nuestra historia",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "movie_clip8",
@@ -102,7 +114,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Top 20 saddest moments in anime",
             "num_rep": 4567,
-            "description": "The 20 moments in the anime that make cry us in some moments"
+            "description": "The 20 moments in the anime that make cry us in some moments",
+            "media": "movie_clip2.mp4"
         },
         {
             "id": "movie_clip9",
@@ -151,7 +164,8 @@ const media = {
             Kimi to iu mirai ni 'kouun' wo!
             Sono chi no sadame jojo
             
-            (Jojo! Jojo! Jojo!)`
+            (Jojo! Jojo! Jojo!)`,
+            "media": "movie_clip3.mp4"
         },
         {
             "id": "movie_clip10",
@@ -159,7 +173,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Hashcode 2020 winners",
             "num_rep": 235,
-            "description": "Highlights Code Competition GoogleHashCode2020"
+            "description": "Highlights Code Competition GoogleHashCode2020",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "movie_clip11",
@@ -167,7 +182,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Atrapados en un ascensor",
             "num_rep": 3,
-            "description": "Tres personas, un mono i una biblia atrapados en un ascensor"
+            "description": "Tres personas, un mono i una biblia atrapados en un ascensor",
+            "media": "movie_clip2.mp4"
         },
         {
             "id": "audio_clip6",
@@ -175,7 +191,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Cumbia barata",
             "num_rep": 264,
-            "description": "Los temas de cumbia mas escuchados por los pobres que no se pueden permitir una buena musica"
+            "description": "Los temas de cumbia mas escuchados por los pobres que no se pueden permitir una buena musica",
+            "media": "audio_clip2.mp3"
         },
         {
             "id": "movie_clip12",
@@ -183,7 +200,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Que pasa si mezclamos cola con mentos?",
             "num_rep": 246,
-            "description": "Que explota, claro"
+            "description": "Que explota, claro",
+            "media": "movie_clip3.mp4"
         },
         {
             "id": "movie_clip13",
@@ -191,7 +209,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Youtube rewind español 2019",
             "num_rep": 86,
-            "description": "Video resumen de las mayores celebridades en YouTube durante el año 2019"
+            "description": "Video resumen de las mayores celebridades en YouTube durante el año 2019",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "audio_clip7",
@@ -199,7 +218,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Rock",
             "num_rep": 34,
-            "description": "La cancion mas escuchada de rock en 1992"
+            "description": "La cancion mas escuchada de rock en 1992",
+            "media": "audio_clip3.mp3"
         },
         {
             "id": "audio_clip8",
@@ -288,7 +308,8 @@ const media = {
             Nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan
             Nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan
             Nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan nyan
-            `
+            `,
+            "media": "audio_clip1.mp3"
         },
         {
             "id": "movie_clip14",
@@ -296,7 +317,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Top 10 jugarretas en bloodborne",
             "num_rep": 577,
-            "description": "Recopilacion de trolleos en el juego Bloodborne"
+            "description": "Recopilacion de trolleos en el juego Bloodborne",
+            "media": "movie_clip3.mp4"
         },
         {
             "id": "movie_clip15",
@@ -304,7 +326,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Las pizzas del futuro",
             "num_rep": 90,
-            "description": "Pizza de pasta de dientes, pizza de peine, pizza de humus, pizza de python, pizza de acero, entre otras"
+            "description": "Pizza de pasta de dientes, pizza de peine, pizza de humus, pizza de python, pizza de acero, entre otras",
+            "media": "movie_clip1.mp4"
         },
         {
             "id": "audio_clip9",
@@ -312,7 +335,8 @@ const media = {
             "image": "audio-thumbnail.jpg",
             "text": "Los mejores chistes de Juanlu de la decada",
             "num_rep": 467,
-            "description": "Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, JS, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan"
+            "description": "Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, JS, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan, Juanan",
+            "media": "audio_clip2.mp3"
         },
         {
             "id": "movie_clip16",
@@ -320,7 +344,8 @@ const media = {
             "image": "poster.jpg",
             "text": "Como instalar Jquery en 2020 (Link de descarga en la descripción, Yo no lo descargo porque ya lo tengo, megaupload 720p 64fps .exe)",
             "num_rep": 46,
-            "description": "Es broma, instala react i a seguir"
+            "description": "Es broma, instala react i a seguir",
+            "media": "movie_clip2.mp4"
         },
 
     ]
